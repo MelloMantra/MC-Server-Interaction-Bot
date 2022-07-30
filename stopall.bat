f5cad508d28c0e0e1c588c5be5c354df089f09b7
Mello Mantra <67211433+MelloMantra@users.noreply.github.com>
2022-07-30 16:11:22 -0700

    Update stopall.bat
    Add log function

--- stopall.bat
+++ stopall.bat
@@ -4,6 +4,7 @@ mcrcon -p passcode -w 20 "say Manual shutdown initiated; server stopping in 20 s
 mcrcon -p passcode stop
 timeout /t 3 /nobreak
 del on.txt
+echo Server stopped (manually) on %date% at %time%. >>log.txt
 taskkill /f /im cmd.exe
 taskkill /f /im cmd.exe
 exit
