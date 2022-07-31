84d929e4d54f32ee3c00c82dbdef6397b890441b
Mello Mantra <67211433+MelloMantra@users.noreply.github.com>
2022-07-30 18:42:25 -0700

    Update startmcserver.bat

--- startmcserver.bat
+++ startmcserver.bat
@@ -1,6 +1,13 @@
 @echo off
+title "Server CLI"
 set RAM=4
 
 :: start server
 cd "C:\Users\mgdr0\Desktop\Misc\Minecraft Server"
 java -Xmx%RAM%G -Xms%RAM%G -jar server.jar
+
+:: detect if server off
+tasklist |findstr /ibc:"java.exe" || (
+    del on.txt
+    exit
+)
