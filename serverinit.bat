586af94699cba7142e8d520254cdbbdd7561b7f3
Mello Mantra <67211433+MelloMantra@users.noreply.github.com>
2024-05-07 22:04:18 -0700

    Delete serverinit.bat

--- serverinit.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd "C:\Users\mgdr0\Desktop\Code\MC Server starter"
-echo Server started on %date% at %time%. >>log.txt
-echo. >>on.txt
-start /min rcon.bat
-start /min /wait startmcserver.bat
-exit
